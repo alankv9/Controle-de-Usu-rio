d5deaf2baa262144ad8277d641209ba480d7dc30
alan.rodrigues <alan.dr2003@gmail.com>
2025-04-08 14:13:20 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
-use App\Http\Controllers\{
-    CadastrarController, ControllerPdf, ControllerPdfEmpres,
-    EmpresaController, UserController, LoginController,
-
-
-};
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    LoginController, CadastrarController, UserController,
+    EmpresaController, ControllerPdf, ControllerPdfEmpres
+};
 
+// Rotas públicas
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login/process', [LoginController::class, 'loginProcess'])->name('login.process');
 Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
@@ -15,21 +13,22 @@ Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 Route::get('/cadastrar', [CadastrarController::class, 'index'])->name('cadastrar');
 Route::post('/cadastrar/process', [CadastrarController::class, 'cadastrarProcess'])->name('cadastrar.process');
 
-// Agrupamento de Rotas Protegidas (Apenas para usuários autenticados)
+// Rotas protegidas por autenticação
 Route::middleware('auth')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('users.index');
     Route::get('/home', [EmpresaController::class, 'index'])->name('empresa.index');
-    
-    // Rotas de Recursos (CRUD)
+
+    // Recursos
     Route::resources([
         'users' => UserController::class,
-        'empresa' => EmpresaController::class
+        'empresa' => EmpresaController::class,
     ]);
 
+    // Empresa - Cadastro de usuário
     Route::get('/empresa/cadastra/usuario', [EmpresaController::class, 'prossEmpre'])->name('empresa.usuario');
-    Route::post('/cadastra/usuario/empresa',[EmpresaController::class, 'cadastraUsuarioEmpresa'])->name('cd');
+    Route::post('/cadastra/usuario/empresa', [EmpresaController::class, 'cadastraUsuarioEmpresa'])->name('cd');
 
-    // Geração de PDFs
+    // PDFs
     Route::get('/users/{id}/pdf', [ControllerPdf::class, 'processPdf'])->name('users.pdf');
     Route::get('/empresa/{id}/pdf', [ControllerPdfEmpres::class, 'processPdfE'])->name('empresas.pdf');
 });
